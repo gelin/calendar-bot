d964032a0d3c204e09246da83bd11cfbff395534
gelin <gelin@gelin-zenbook>
2016-08-07 23:46:26 +0600

    Implemented saving and reading of the last notification for each calendar event.

--- calbot/conf.py
+++ calbot/conf.py
@@ -87,12 +87,14 @@ var/
     ...
 ```
 """
-
+from configparser import ConfigParser
 import logging
+import os
 from datetime import time
 
 TOKEN = '225478221:AAFvpu4aBjixXmDJKAWVO3wNMjWFpxlkcHY'
 CALENDAR_URL = 'https://calendar.google.com/calendar/ical/aqclsibjm591jbbk875uio9k40%40group.calendar.google.com/public/basic.ics'
+# CALENDAR_URL = 'https://calendar.google.com/calendar/ical/rvsmtm05j6qc2126epnngu9kq0%40group.calendar.google.com/private-5d15121a99e8d543ae656471323b26e7/basic.ics'
 # TODO read from config files
 
 
@@ -100,6 +102,10 @@ __all__ = ['Config']
 
 
 logger = logging.getLogger('conf')
+
+
+def events_file(vardir, user_id, cal_id):
+    return os.path.join(vardir, user_id, cal_id, 'events.cfg')
 
 
 class Config:
@@ -120,7 +126,7 @@ class Config:
         Returns list of all known and monitoring calendars
         :return: list of CalendarConfig
         """
-        return [CalendarConfig(CALENDAR_URL)]   # TODO more calendars
+        return [CalendarConfig(self, CALENDAR_URL)]   # TODO more calendars
 
 
 class CalendarConfig:
@@ -128,20 +134,68 @@ class CalendarConfig:
     Current calendar state.
     """
 
-    def __init__(self, url):
+    def __init__(self, config, url):
+        self.vardir = config.vardir
+        """Base var directory"""
+        self.id = '1'
+        """Current calendar ID"""
+        self.user_id = 'TEST'
+        """Chat ID of the user to whom this calendar belongs to"""
         self.url = url
         """url of the ical file to download"""
         self.advance = [24, 48]     # TODO read from files
         """array of the numbers: how many hours in advance notify about the event"""
-        self.day_start = time(10, 0)    # when the day starts when the event has no specified time
+        self.day_start = time(10, 0)
+        """when the day starts if the event has no specified time"""
+        self.events = {}
+        """dictionary of known configured events"""
+        self.load_events()
+
+    def load_events(self):
+        config = ConfigParser()
+        config.read(events_file(self.vardir, self.user_id, self.id))
+        for event_id in config.sections():
+            event = EventConfig(self, event_id)
+            event.last_notified = config.getint(event_id, 'last_notified', fallback=None)
+            self.events[event_id] = event
 
     def event(self, id):
         """
-        Returns the calendar event by it's id
+        Returns the persisted state of calendar event by it's id
         :param id: id of the event
-        :return: the EventConfig instance
+        :return: the EventConfig instance, read from persisted storage or a new one
         """
-        return EventConfig()        # TODO read from files
+        try:
+            return self.events[id]
+        except KeyError:
+            event = EventConfig(self, id)
+            self.events[id] = event
+            return event
+
+    def event_notified(self, event):
+        """
+        Marks the event in config as notified.
+        Copies data from the ical event object
+        :param event: runtime event processed by ical module
+        :return: None
+        """
+        config_event = self.event(event.id)
+        config_event.last_notified = event.notified_for_advance
+
+    def save_events(self):
+        """
+        Saves all tracked events into persisted file
+        :return: None
+        """
+        filename = events_file(self.vardir, self.user_id, self.id)
+        caldir = os.path.dirname(filename)
+        os.makedirs(caldir, exist_ok=True)
+        with open(filename, 'wt') as file:
+            config = ConfigParser()
+            for event in self.events.values():
+                config.add_section(event.id)
+                config.set(event.id, "last_notified", str(event.last_notified))
+            config.write(file)
 
 
 class EventConfig:
@@ -149,8 +203,12 @@ class EventConfig:
     Current calendar event state.
     """
 
-    def __init__(self):
-        self.id = 1                 # TODO save and read from files
+    def __init__(self, calendar, id):
+        self.id = id
         """the event id, as it was read from the ical file"""
-        self.last_notified = None   # TODO save and read from files
+        self.cal_id = calendar.id
+        """ID of the calendar to which this event belongs to"""
+        self.user_id = calendar.user_id
+        """Chat ID of the user to whom the calendar belongs to"""
+        self.last_notified = None
         """the last notification made for this event, as hours in advance, the integer or None"""
